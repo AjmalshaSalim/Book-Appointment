46fd508e8cac568b8fba6d94052a8a0d32610416
ajmal <ajmalaj8085@gmail.com>
2023-12-05 01:23:09 +0530

    nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header bg-white text-black w-full flex shadow-md">
+      <div className="leftSide w-1/2">
+      <div className="logoContainer rounded-full text-white">
+        <p className="profile text-lg ">MD</p>
+      </div>
+      </div>
+      <div className="rightSide w-1/2 text-right">
+      <div className="linkText">
+        Help ?
+        </div>
+        </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
